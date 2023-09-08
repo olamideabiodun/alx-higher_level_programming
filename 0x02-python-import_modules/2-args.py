0c5eb765cec86d3a30b6a7ddb9af40fe2355cf39
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-08 07:44:28 +0300

    print infinite

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
     count = len(sys.argv) - 1
     if count == 0:
         print("0 argument")
@@ -9,5 +8,5 @@ if __name__ == "__main__":
         print("1 argument")
     else:
         print("{} arguments".format(count))
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
+        for i in range(count):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
