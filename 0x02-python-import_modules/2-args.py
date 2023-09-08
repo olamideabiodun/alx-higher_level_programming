87c6b2cc018390a176f280963ed88a705d11d6a9
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-08 07:15:42 +0300

    prints args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,5 +2,12 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 argument")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(count))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
