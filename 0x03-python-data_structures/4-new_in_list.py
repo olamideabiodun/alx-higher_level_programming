52ca71236179109956a20ab7087e026259ce31bf
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-10 12:46:34 +0300

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     If there is already something in that position it will be replaced."""
     # TODO: Implement this function!
     copy = my_list.copy()
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         copy[idx] = element
