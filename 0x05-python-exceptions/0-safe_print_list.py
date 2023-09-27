567d75f5ba9a5db8228a97ece23a4ec02fb6ba73
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-09-27 17:39:00 +0300

    print index

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """print elemnt x of a list
+    Args:
+    my_list (list, optional) : the list to print. Defaults to [].
+    x (int, optional) : index in the list. Defaults to 0.
+    
+    Returns:
+    x element of my_list
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
