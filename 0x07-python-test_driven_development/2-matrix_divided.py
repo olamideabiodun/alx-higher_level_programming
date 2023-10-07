c5cb9086a4cb95d081bf3cc8fc9aa428aff24389
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-07 11:57:57 +0300

    name test passed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,8 +6,10 @@ def matrix_divided(matrix, div):
     """Divides all elements of a given matrix by a number."""
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
-            not all(isinstance(ele, (int, float)) for row in matrix for ele in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            not all(isinstance(ele, (int, float))
+                    for row in matrix for ele in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of"
+                        " integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
