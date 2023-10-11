20b93aba215e4001005516cdbb27e526ac4eaf68
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-11 08:21:19 +0300

    load, save and add

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Save object to file function"""
+
+
+def save_to_json_file(my_obj, filename):
+    """write an object to a text file using JSON representation"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
