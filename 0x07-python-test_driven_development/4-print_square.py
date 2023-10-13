1a0e8cf9c4a3c3021bd2b89279e7d39428006e88
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-13 15:16:43 +0300

    print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A function that prints square"""
+
+
+def print_square(size):
+    """The function"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range (size)]
+        print("")
