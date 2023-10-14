1a78ade87c0469a3da4226c80929efa73c3fe9c7
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-14 17:17:24 +0300

    Unit Tested

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,5 +7,14 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_one(self):
         self.assertEqual(max_integer([1, 2]), 2)
-        if __name__ == '__main__':
-            unittest.main()
+        self.assertEqual(max_integer([1, 6, 4,]), 6)
+        self.assertEqual(max_integer([6, 4]), 6)
+        self.assertEqual(max_integer([6, 4, 5]), 6)
+        self.assertEqual(max_integer([1, 3, 7]), 7)
+
+    def test_empty_input(self):
+        result = max_integer([])
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main()
