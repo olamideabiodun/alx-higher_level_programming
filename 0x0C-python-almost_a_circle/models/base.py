d61721382d1da0d02a80aa4b0235059d5d2dec65
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-10-16 14:01:51 +0300

    Base Class Created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Base model class"""
+
+
+class Base:
+    """Base Model"""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """Initializes a new base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
