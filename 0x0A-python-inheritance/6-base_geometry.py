211d7def4a615440a6dcc464c918afb21f5ce4e1
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-12-05 00:29:48 +0200

    6geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""based on Base-geometry class"""
+
+
+class BaseGeometry:
+    """A base_geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    pass
