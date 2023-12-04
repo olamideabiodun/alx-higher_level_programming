c5d9a53616d59778e53ac15e35be305bf12cd829
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-12-05 00:52:32 +0200

    geometry tested

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""based on Base-geometry class"""
+
+
+class BaseGeometry:
+    """A base_geometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    pass
+
+    def integer_validator(self, name, value):
+        """integer validator"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
